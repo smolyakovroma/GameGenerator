59257c7c662da0a1301ff9e318c64cef0f196fb3
romansmolakov <smolyakovroma@gmail.com>
2018-11-12 03:41:11 +0700

    add cards

---
+++ app/src/main/java/com/romansmolakov/solgame/Constants.java
@@ -0,0 +1,8 @@
+package com.romansmolakov.solgame;
+
+public class Constants {
+
+    public static final int TOP_MARGIN = 90;
+    public static final int RANGE_STACK_VERTICAL = 30;
+    public static final int RANGE_STACK_HORIZONTAL = 60;
+}
